f860577eb6c9ffee15470263f0b6d85ae935b7e4
Bonnie-TJ <e1335039@u.nus.edu>
2024-08-27 15:07:56 +0800

    update user-service for GFO-32_WebCreateGroupOrder

--- src/main/java/com/nus/edu/se/user_service/dao/UserRepository.java
+++ src/main/java/com/nus/edu/se/user_service/dao/UserRepository.java
@@ -4,6 +4,7 @@ import com.nus.edu.se.user_service.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -11,4 +12,6 @@ public interface UserRepository extends JpaRepository<Users, UUID> {
     Users findByName(String name);
 
     Users findByEmail(String email);
+
+//    Optional<Users> findById(UUID user_id);
 }
