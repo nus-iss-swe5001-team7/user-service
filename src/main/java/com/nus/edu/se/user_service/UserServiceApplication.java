5188ca46a829a5288243bda1f3b689370caee71e
tronlink-dev-2024 <tronlinkdev2024@gmail.com>
2024-10-14 17:43:16 +0800

    update eureka config

--- src/main/java/com/nus/edu/se/user_service/UserServiceApplication.java
+++ src/main/java/com/nus/edu/se/user_service/UserServiceApplication.java
@@ -1,9 +1,14 @@
 package com.nus.edu.se.user_service;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -11,8 +16,26 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class UserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(UserServiceApplication.class, args);
+  }
+
+
+  @Bean
+  public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+
+    EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+    String ip = null;
+    try {
+      ip = InetAddress.getLocalHost().getHostAddress();
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+
+    config.setIpAddress(ip);
+    config.setPreferIpAddress(true);
+
+    return config;
+  }
 
 }
